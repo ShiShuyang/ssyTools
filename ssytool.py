8a1bc6c2135095fb2c5f2b0c1908f3734bfee08c
施舒阳 <290475975@qq.com>
2016-05-10 17:01:41 +0800

    123

--- ssytool.py
+++ ssytool.py
@@ -1,11 +1,52 @@
 # -*- coding: utf8 -*-
 
+import urllib2
+
 def utf82gbk(s):
     return s.decode('utf8').encode('gbk')
 
 def gbk2utf8(s):
     return s.decode('gbk').encode('utf8')
 
+def openurl(url, timeoutTime = 10 ,timeoutFlag = True):
+    if timeoutFlag:
+        try:
+            return urllib2.urlopen(url, timeout = timeoutTime).read()
+        except:
+            return openurl(url, timeloutTime, True)		
+    else:
+        return urllib2.urlopen(url, timeout = timeoutTime).read()
+
+def readText(filename, mode = 'r'):
+    f = open(filename, mode)
+    c = f.read()
+    f.close()
+    return c
+
+def readLines(filename, strip = True, mode = 'r'):
+    f = open(filename, mode)
+    c = f.readlines()
+    f.close()
+    if strip:
+        return [i.strip() for i in c]
+    else:
+        return c
+
+def readCellLines(filename, split, mode = 'r'):
+    f = open(filename, mode)
+    c = f.readlines()
+    f.close()
+    return [i.strip().split(split) for i in c]
+
+def appendText(filename, text, mode = 'a'):
+    f = open(filename, mode)
+    f.write(text)
+    f.close()
+
+def writeText(filename, text, mode = 'w'):
+    f = open(filename, mode)
+    f.write(text)
+    f.close()    
 
 if __name__ == '__main__':
     print utf82gbk('中文')
