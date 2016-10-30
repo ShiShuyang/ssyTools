b8ea75a1c7c802ed60d8aff4b0dd842e6d3707af
施舒阳 <290475975@qq.com>
2016-10-30 13:28:50 +0800

    日常小更新

--- ssytool.py
+++ ssytool.py
@@ -1,12 +1,10 @@
 # -*- coding: utf8 -*-
+import urllib2, os
 
-import urllib2
+__version__ = '161030'
 
-def utf82gbk(s):
-    return s.decode('utf8').encode('gbk')
-
-def gbk2utf8(s):
-    return s.decode('gbk').encode('utf8')
+def utf82gbk(s): return s.decode('utf8').encode('gbk')
+def gbk2utf8(s): return s.decode('gbk').encode('utf8')
 
 def openurl(url, timeoutTime = 10 ,timeoutFlag = True):
     if timeoutFlag:
@@ -32,11 +30,14 @@ def readLines(filename, strip = True, mode = 'r'):
     else:
         return c
 
-def readCellLines(filename, split, mode = 'r'):
+def readCellLines(filename, spliter = ',', structure = None, mode = 'r'):
     f = open(filename, mode)
     c = f.readlines()
     f.close()
-    return [i.strip().split(split) for i in c]
+    if structure:
+        return [[j(k) for j, k in zip(structure, i.strip().split(spliter))] for i in c]
+    else:
+        return [i.strip().split(spliter) for i in c]
 
 def appendText(filename, text, mode = 'a'):
     f = open(filename, mode)
@@ -48,6 +49,24 @@ def writeText(filename, text, mode = 'w'):
     f.write(text)
     f.close()    
 
-if __name__ == '__main__':
+def iterCellLines(filename, spliter = ',', structure = None, mode = 'r'):
+    with open(filename, mode) as f:
+        for line in iter(f.readline, ''):
+            if structure:
+                yield [j(k) for j, k in zip(structure, line.strip().split(spliter))]
+            else:
+                yield line.strip().split(spliter)
+    
+def iterLines(filename, strip = True, mode = 'r'):
+    with open(filename, mode) as f:
+        for line in iter(f.readline, ''):
+            yield line.strip() if strip else line
+
+def setup(): os.system('copy ssytool.py C:\Python27\Lib\site-packages\ssytool.py /Y')
+
+if __name__ == '__main__':    
     print utf82gbk('中文')
-    print 'miao'
+    print readCellLines('test.txt', ',', [str, int, float, str])
+    for line in iterCellLines('test.txt', structure = [str, int, float, str]):
+        print line
+    print '====='
